471f54cd0b06d096afb3e5695a3763a2d94b51f5
aoifem00 <amcmanu4@binghamton.edu>
2021-09-17 14:57:33 -0400

    fixed function declarations

--- Deque.hpp
+++ Deque.hpp
@@ -6,11 +6,17 @@ struct Iterator{
   int index;
 }
 
-#define Deque_DEFINE(t)
-  struct Deque_##var{
-    void (*push_front)(var);
-    void (*push_back)(var);
-  };
-  void Deque_##t##_ctor(Deque_##t *p, int i) {
-    p = (Deque_##t *) malloc(i);
-  }
+#define Deque_DEFINE(t)                                                        \
+    struct Deque_##t {                                                         \
+        t type_name;                                                           \
+        void (*push_front)(Deque_##t *deq, t item);                            \
+        void (*push_back)(Deque_##t *deq, t item);                             \
+        void (*dtor)(Deque_##t *p);                                            \
+    };                                                                         \
+    void Deque_##t##_dtor(Deque_##t *p){                                       \
+    }                                                                          \
+    void Deque_##t##_ctor(Deque_##t *p, int i) {                               \
+      p = (Deque_##t *) malloc(i);                                             \
+      p->type_name=t;                                                          \
+      p->dtor=Deque_##t##_dtor;                                                \
+    }                                                                          \

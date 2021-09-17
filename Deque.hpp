efc773289164605b3215afbb0df74424a4bdfae3
aoifem00 <amcmanu4@binghamton.edu>
2021-09-17 14:18:08 -0400

    added constructor function

--- Deque.hpp
+++ Deque.hpp
@@ -2,14 +2,15 @@ struct Container{
   String* arr;
 };
 
-struct Iter{
-  unsigned long address;
+struct Iterator{
+  int index;
 }
 
-
-#define Deque_DEFINE(var)
-    struct Deque_##var{
-      void (*push_front)(var);
-      void (*push_back)(var);
-    };
-    
+#define Deque_DEFINE(t)
+  struct Deque_##var{
+    void (*push_front)(var);
+    void (*push_back)(var);
+  };
+  void Deque_##t##_ctor(Deque_##t *p, int i) {
+    p = (Deque_##t *) malloc(i);
+  }

b5e30f2d6cc5f90a046de4f561ed8a6425a8797e
aoifem00 <amcmanu4@binghamton.edu>
2021-09-20 14:42:18 -0400

    compiled test code

--- Deque.hpp
+++ Deque.hpp
@@ -1,62 +1,79 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define Deque_DEFINE(t)                                                        \
     struct Deque_##t##_Iterator {                                              \
       t type;                                                                  \
       t* pointer;                                                              \
       t& reference;                                                            \
-      Deque_##t##_Iterator (*inc)(Deque_##t##_Iterator* it);                   \
-      t (*deref)(Deque_##t##_Iterator *it);                                    \
+      void (*inc)(Deque_##t##_Iterator* it);                                   \
+      void (*dec)(Deque_##t##_Iterator* it);                                   \
+      t& (*deref)(Deque_##t##_Iterator*);                                      \
     };                                                                         \
     struct Deque_##t {                                                         \
       t* data;                                                                 \
       t type_name;                                                             \
-      void (*push_front)(Deque_##t *deq, t item);                              \
-      void (*push_back)(Deque_##t *deq, t item);                               \
-      void (*dtor)(Deque_##t *p);                                              \
-      t (*front)(Deque_##t *p);                                                \
-      t (*back)(Deque_##t *p);                                                 \
+      int sz;                                                                  \
+      void (*push_front)(Deque_##t*, t);                                       \
+      void (*push_back)(Deque_##t*, t);                                        \
+      void (*dtor)(Deque_##t*);                                                \
+      t &(*front)(Deque_##t*);                                                 \
+      t &(*back)(Deque_##t*);                                                  \
       void (*pop_front)(Deque_##t *p);                                         \
       void (*pop_back)(Deque_##t *p);                                          \
-      int (*size)(Deque_##t *p);                                               \
-      t (*at)(Deque_##t *p, int i);                                            \
+      size_t& (*size)(Deque_##t *p);                                           \
+      t& (*at)(Deque_##t *p, int i);                                           \
       void (*clear)(Deque_##t *p);                                             \
       bool (*empty)(Deque_##t *p);                                             \
-      void (*ctor)(t* t1, bool b);                                             \
       Deque_##t##_Iterator (*begin)(Deque_##t *deq);                           \
       bool (*Deque_##t##_equal)(t item1, t item2);                             \
       void (*sort)(Deque_##t *deq, Deque_##t##_Iterator it1, Deque_##t##_Iterator it2); \
       Deque_##t##_Iterator (*end)(Deque_##t *deq);                             \
     };                                                                         \
-    void Deque_##t##_Iterator_equal(Deque_##t##_Iterator it1, Deque_##t##_Iterator it2){ \
-                                                                               \
+    Deque_##t##_Iterator* Deque_##t##_Iterator_new(){                           \
+      Deque_##t##_Iterator* iter=(Deque_##t##_Iterator*) malloc(sizeof(Deque_##t##_Iterator)); \
+      return iter;                                                             \
     }                                                                          \
-    t deref(Deque_##t##_Iterator* it){                                         \
-      return (t)(*it);                                                         \
+    bool Deque_##t##_Iterator_equal(Deque_##t##_Iterator it1, Deque_##t##_Iterator it2){ \
+      return true;                                                             \
     }                                                                          \
-    Deque_##t##_Iterator inc(Deque_##t##_Iterator* it){                        \
-      return &(it+sizeof(it->type));                                           \
+    t& deref(Deque_##t##_Iterator* it){                                         \
+      return *(it->pointer);                                                   \
     }                                                                          \
-    t at(Deque_##t *p, int i){                                                 \
-      return (p->data+i);                                                      \
+    void inc(Deque_##t##_Iterator* it){                                        \
+      it=it+sizeof(it->type);                                                  \
+    }                                                                          \
+    void dec(Deque_##t##_Iterator* it){                                        \
+      it=it-sizeof(it->type);                                                  \
+    }                                                                          \
+    t& Deque_##t##_at(Deque_##t *p, int i){                                    \
+      return *(p->data+i);                                                     \
     }                                                                          \
     void sort(Deque_##t *deq, Deque_##t##_Iterator it1, Deque_##t##_Iterator it2){ \
     }                                                                          \
     Deque_##t##_Iterator begin(Deque_##t *p){                                  \
-      return (unsigned long)(&p->data);                                        \
+      Deque_##t##_Iterator* iter=Deque_##t##_Iterator_new();                   \
+      iter->pointer=p->data;                                                   \
+      return (*iter);                                                          \
     }                                                                          \
     void Deque_##t##_dtor(Deque_##t *p){                                       \
     }                                                                          \
-    t Deque_##t##_front(Deque_##t *p){                                         \
+    t& Deque_##t##_front(Deque_##t *p){                                        \
       return *(p->data);                                                       \
     }                                                                          \
-    t Deque_##t##_back(Deque_##t *p){                                          \
-      return *(p->data+size);                                                  \
+    t& Deque_##t##_back(Deque_##t *p){                                         \
+      return *(p->data+p->sz);                                                 \
     }                                                                          \
     void Deque_##t##_pop_front(Deque_##t *p){                                  \
     }                                                                          \
     void Deque_##t##_pop_back(Deque_##t *p){                                   \
     }                                                                          \
-    int Deque_##t##_size(Deque_##t *p){                                        \
-      return 0;                                                                \
+    size_t& Deque_##t##_size(Deque_##t *p){                                    \
+      size_t* n=(size_t*)malloc(sizeof(size_t));                               \
+      *n=0;                                                                    \
+      return *n;                                                               \
     }                                                                          \
     void Deque_##t##_clear(Deque_##t *p){                                      \
     }                                                                          \
@@ -64,6 +81,10 @@
       return true;                                                             \
     }                                                                          \
     void Deque_##t##_ctor(Deque_##t *deq, bool b){                             \
+      deq = (Deque_##t*) malloc(sizeof(Deque_##t));                            \
+      deq->front=&Deque_##t##_front;                                           \
+      deq->back=&Deque_##t##_back;                                             \
+      deq->at=&Deque_##t##_at;                                                 \
     }                                                                          \
     bool Deque_##t##_equal(Deque_##t deq1, Deque_##t deq2){                    \
       return true;                                                             \
